bc350b2c36f99ccf0ec2f139bde409d15aca26cc
Alexander <alexchasejones@icloud.com>
2017-09-18 18:14:48 -0400

    updates

--- swoosh/swoosh/ViewController.swift
+++ swoosh/swoosh/ViewController.swift
@@ -12,10 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        bgImg.frame = view.frame
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func unwindFromSkillVC(unwindSeque: UIStoryboardSegue) {
+        
     }
 }
 
